877f95e9909637be7e641270d78e13f868c04f41
Zimakova Yulia <Zimakova86@yandex.ru>
2022-06-13 22:36:03 +0300

    add task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 7;
+int b = 100;
+int c = 99;
+int max = a;
+ 
+if (b > a)
+{ 
+    max = b;
+}
+if (c > b)
+{
+    max = c
+}
+
+Console.WriteLine($"{max} - максимальное из трех");
