2f4b001246e6659ec361a600cf94299361971ebc
Zimakova Yulia <Zimakova86@yandex.ru>
2022-06-13 22:34:50 +0300

    add all

--- method/Program.cs
+++ method/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] array = {1, 12, 31, 4, 15, 16, 17, 18, 19 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
+//index = index + 1;
+
+
