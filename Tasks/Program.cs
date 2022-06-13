d97d87b0362e94b8c7a57dc2ba24bacc575f0186
Zimakova Yulia <Zimakova86@yandex.ru>
2022-06-13 22:29:00 +0300

    add Task and task2

---
+++ Tasks/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 16;
+int b = 160;
+int max;
+int min;
+if (a > b)
+{
+    max = a;
+    min = b;
+    Console.WriteLine($"{max} - большее, {min} - меньшее");
+}
+else if (a < b)
+{
+    max = b;
+    min = a;
+    Console.WriteLine($"{max} - большее, {min} - меньшее");
+}
+else
+{
+Console.WriteLine("Числа равны равны между собой ");
+}
