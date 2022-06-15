3537a1808b94a7b0a289ea16765902b0cc3b3369
Zimakova Yulia <Zimakova86@yandex.ru>
2022-06-15 22:06:16 +0300

    added task10and method3

---
+++ Task10/Program.cs
@@ -0,0 +1,32 @@
+﻿void CoordinatesSearch(int X, int Y)
+{
+    if (X== 0 || Y== 0)
+    {
+        System.Console.WriteLine("Параметры не корректны");
+    }
+    else
+    {
+        if (X > 0 && Y > 0)
+        {
+            Console.WriteLine("1");
+        }
+        if (X > 0 && Y < 0)
+        {
+            Console.WriteLine("4");
+        }
+        if (X < 0 && Y > 0)
+        {
+            Console.WriteLine("2");
+        }
+        if (X < 0 && Y < 0)
+        {
+            Console.WriteLine("3");
+        }
+    }
+}
+CoordinatesSearch(2, -2);
+CoordinatesSearch(-2, -2);
+CoordinatesSearch(-2, 2);
+CoordinatesSearch(0, 0);
+CoordinatesSearch(2, 2);
+
---
+++ method3/Program.cs
@@ -0,0 +1,31 @@
+﻿void Dotlocation (int a)
+{
+    if (a > 4)
+    {
+        Console.WriteLine("Параметры не корректны");
+    }
+    else
+    {
+        if (a == 1) 
+    
+        {
+            Console.WriteLine ("X > 0, Y > 0");
+        }
+        if (a == 4)
+        {
+            Console.WriteLine("X > 0, Y < 0");
+        }
+        if (a == 2)
+        {
+            Console.WriteLine("X < 0, Y > 0");
+        }
+        if (a == 3)
+        {
+            Console.WriteLine("X < 0, Y < 0");
+        }
+    }
+}
+Dotlocation(1);
+Dotlocation(2);
+Dotlocation(3);
+Dotlocation(4);
