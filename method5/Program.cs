3c74b9c1d1b74a01c6d8c7f5b4082d0c255389e5
Zimakova Yulia <Zimakova86@yandex.ru>
2022-06-15 23:43:40 +0300

    method4 and method5 added

---
+++ method5/Program.cs
@@ -0,0 +1,7 @@
+﻿void Distance3D ( int Ax, int Ay, int Az, int Bx, int By, int Bz )
+{
+    double AB = Math.Sqrt((Math.Pow((Bx-Ax),2))+(Math.Pow((By-Ay),2))+(Math.Pow((Bz-Az),2)));
+    Console.WriteLine(AB);
+}
+
+Distance3D (4, 10, 18, 1, -1, -6);
